85fc72b27f701c5dc5aeecf9e0c1102edd60b0d1
Carla Hoppenstedt <carlahopp94@gmail.com>
2026-09-10 17:01:11 -0600

    Agregar gitignore para Windows Python y Node

---
+++ ejercicio1.py
@@ -0,0 +1,34 @@
+"#Python es un lenguaje de alto nivel, orientado a objetos, lenguaje interpretado y de tipado dinámico."
+#Variables : Son espacios de memoria que se utilizan para almacenar distintos tipos de datos.
+#Cuatro tipos de datos simples
+
+#string : cadena de texto, se escribe con "", '',"""""",''''.
+
+#Las comillas simples y dobles se utilizan en textos de una sola línea
+#Las comillas triples se utilizan en textos multiples
+
+
+edad = "32"
+nombre = "Carla"
+
+#Numeros flotantes (punto decimal)
+34.4
+
+#Boleanos : Se dividen en True o False (se escribe con mayuscula)
+True
+False   
+
+saludo = "Hola "+ nombre +  " Bienvenida"
+print(saludo)
+
+#Ejercicio Input/almaceno datos/output
+nombre = input("¿Cuál es tu nombre?: ")
+edad = input("¿Qué edad tienes?: ")
+ch = input ("¿Cual es tu cohorte?: ")
+dato_desconocido = input("¿Cuentame un dato muy secreto?: ")
+dato_random = input("Ahora cuentame un dato random: ")
+
+print(nombre + edad + ch + dato_desconocido + dato_random)
+
+#print(nombre + edad + ch + dato_desconocido + dato_random)
+
---
+++ hello.js
@@ -0,0 +1,7 @@
+console.log("hello world")
+console.log("Soy Carla");
+console.log("Estoy aprendiendo Java");
+console.log("Quiero crear una página web");
+
+let name = "Carla";
+console.log("hello world", name);
