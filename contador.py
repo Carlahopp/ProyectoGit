80c5c29c7b07d65bcfeacf1f9fef8ed72035f708
Carla Hoppenstedt <carlahopp94@gmail.com>
2026-09-14 17:14:52 -0600

    Agregados los 5 archivos del proyecto de FizzBuzz

---
+++ contador.py
@@ -0,0 +1,8 @@
+#Actividad: El contador inteligente.
+for número in range(1,11):
+    if número < 5 == 0:
+        print(número,"Número pequeño")
+    elif número == 5:
+        print(número,"Llegamos a 5")
+    else: #Si no es menor a 5, ni es igual a 5... entonces es mayor a 5.
+        print(número,"Número grande")
